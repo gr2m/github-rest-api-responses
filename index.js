12266534d45900798f6424381169682e22766988
Gregor <gregor@martynus.net>
2019-03-18 15:35:50 -0700

    feat: add accept header where a preview is required

--- index.js
+++ index.js
@@ -6,19 +6,28 @@ const ROUTES = require('@octokit/routes')
 
 const requests = Object.keys(ROUTES).reduce((requests, scope) => {
   ROUTES[scope].forEach(endpoint => {
-    const { method, path, params } = endpoint
+    const { method, path, params, previews } = endpoint
     const requestParams = params
       .filter(param => param.required && !/[.\[]/.test(param.name))
       .map(param => param.name)
+    const acceptHeader = previews
+      .filter(preview => preview.required)
+      .map(preview => `application/vnd.${preview.name}`)
+      .join(',')
 
-    requests.push({ method, path, params: requestParams })
+    requests.push(
+      Object.assign(
+        { method, path, params: requestParams },
+        acceptHeader ? { headers: { accept: acceptHeader } } : {}
+      )
+    )
   })
 
   return requests
 }, [])
 
 const methodOrder = ['GET', 'HEAD', 'POST', 'PATCH', 'PUT', 'DELETE']
-const sortedUniqRequests = sortBy(uniqRequests, request => `${request.path}/ ${methodOrder.indexOf(request.method)}`)
+const sortedUniqRequests = sortBy(requests, request => `${request.path}/ ${methodOrder.indexOf(request.method)}`)
 console.log(`${requests.length} requests found`)
 writeFileSync(FILE_PATH, JSON.stringify(sortedUniqRequests, null, 2))
 console.log(`${FILE_PATH} updated.`)
